934813c1f90341c1814708e571288e4825bfed37
Mikayla Kelhofer <mikaylakelhofer@gmail.com>
2017-10-31 10:06:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// http is part of core.  We need it to run createServer
+const http = require('http');
+// We need to bring in the Express module so we can use the router, public folder, etc.
+const express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+// run createServer against http like always but hand it the app (express)
+var server = http.createServer(app);
+server.listen(8080);
+console.log('The server is listening on port 8080');
+
+
